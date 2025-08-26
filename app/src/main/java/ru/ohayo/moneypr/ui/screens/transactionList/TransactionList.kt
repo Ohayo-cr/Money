ef340adffe77b7caf3e97f46ebbccf078db15c55
Admin <leksan.96@mail.ru>
2025-08-26 20:32:20 +0300

    TransactionDetailsScreen ?тормознул пуш

--- app/src/main/java/ru/ohayo/moneypr/ui/screens/transactionList/TransactionList.kt
+++ app/src/main/java/ru/ohayo/moneypr/ui/screens/transactionList/TransactionList.kt
@@ -1,7 +1,6 @@
 package ru.ohayo.moneypr.ui.screens.transactionList
 
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
